03720404b62a8d77c101cee2521674cf6f038b2d
developerTajul <developer.tajul@gmail.com>
2020-07-08 08:17:21 +0600

    basic tasks done

---
+++ js/custom.js
@@ -0,0 +1,160 @@
+(function($){
+
+	jQuery(document).ready(function(){
+
+		$('.main-slider-active').slick({
+		  dots: false,
+		  arrows: false,
+		  infinite: false,
+		  speed: 300,
+		  slidesToShow: 1,
+		  slidesToScroll: 1,
+		});
+
+		$('.testimonial-active').slick({
+		  dots: true,
+		  arrows: false,
+		  infinite: false,
+		  speed: 300,
+		  slidesToShow: 1,
+		  slidesToScroll: 1,
+		});
+
+		$('.brand-active').slick({
+		  dots: true,
+		  arrows: false,
+		  infinite: false,
+		  speed: 300,
+		  slidesToShow: 5,
+		  slidesToScroll: 1,
+		});
+
+/**
+		$('.main-slide-active').owlCarousel({
+		    loop:true,
+		    dots: true,
+		    nav:true,
+		    navText: ['<i class="far fa-angle-left">', '<i class="far fa-angle-right">'],
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:1
+		        },
+		        1000:{
+		            items:1
+		        }
+		    }
+		});
+**/
+
+		$('.project-active').owlCarousel({
+		    loop:true,
+		    dots: false,
+		    nav:true,
+		    navText: ['<i class="far fa-angle-left">', '<i class="far fa-angle-right">'],
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:3
+		        },
+		        1000:{
+		            items:4
+		        }
+		    }
+		});
+
+
+
+		$('.image-popup-vertical-fit').magnificPopup({
+			type: 'image',
+			closeOnContentClick: true,
+			mainClass: 'mfp-img-mobile',
+			image: {
+				verticalFit: true
+			}
+			
+		});
+
+		$('.image-popup-fit-width').magnificPopup({
+			type: 'image',
+			closeOnContentClick: true,
+			image: {
+				verticalFit: false
+			}
+		});
+
+		$('.image-popup-no-margins').magnificPopup({
+			type: 'image',
+			closeOnContentClick: true,
+			closeBtnInside: false,
+			fixedContentPos: true,
+			mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+			image: {
+				verticalFit: true
+			},
+			zoom: {
+				enabled: true,
+				duration: 300 // don't foget to change the duration also in CSS
+			}
+		});
+
+
+		$('.popup-video').magnificPopup({type:'iframe'});
+
+		// latest blog
+		$('.blog-active').owlCarousel({
+		    loop:true,
+		    dots: false,
+		    nav: true,
+		    navText: ['<i class="far fa-angle-left">', '<i class="far fa-angle-right">'],
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:1
+		        },
+		        1000:{
+		            items:1
+		        }
+		    }
+		});
+		
+		// testimonial-active
+		// $('.testimonial-active').owlCarousel({
+		//     loop:true,
+		//     dots: false,
+		//     nav: true,
+		//     navText: ['<i class="far fa-angle-left">', '<i class="far fa-angle-right">'],
+		//     responsive:{
+		//         0:{
+		//             items:1
+		//         },
+		//         600:{
+		//             items:1
+		//         },
+		//         800:{
+		//             items:1
+		//         },
+		//         1000:{
+		//             items:1
+		//         }
+		//     }
+		// });
+
+
+
+
+
+
+
+		
+
+	});
+
+
+}(jQuery));
